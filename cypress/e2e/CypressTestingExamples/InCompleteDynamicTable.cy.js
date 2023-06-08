b70739feb7800ca86b40f470c326630584fb2ff8
SreekanthQATester <sreekanth7075@gmail.com>
2023-06-08 15:58:14 +0530

    second commit

--- cypress/e2e/CypressTestingExamples/InCompleteDynamicTable.cy.js
+++ cypress/e2e/CypressTestingExamples/InCompleteDynamicTable.cy.js
@@ -5,9 +5,9 @@ describe('Notification Message', ()=>{
     it('Notification Message',()=>{
 
         cy.visit('https://practice.expandtesting.com/notification-message-rendered')
-        cy.contains('Click here').click()
-        cy.contains('Action successful').first().should('be.visible')
-        cy.contains('Action successful').first().should('have.text', 'Action successful')
+      //  cy.contains('Click here').click()
+      //  cy.contains('Action successful').first().should('be.visible')
+      //  cy.contains('Action successful').first().should('have.text', 'Action successful')
         
     
     })
